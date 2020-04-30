6dae34d00766444abf996a5164ad87be0b9e534a
jannonx <123456>
2020-04-30 16:07:04 +0800

    初版定型
    添加下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,6 @@ const app = getApp()
 var utils = require('../../utils/utils.js');
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -12,40 +11,24 @@ Page({
     //  n_number:'29.3231565',
     //  n_number:parseFloat(n_number).toFixed(1)
   
-     
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  // onLoad: function (options) {
-  //   var that = this;
-  //   var n_number;
-  //   wx.getSystemInfo({
-  //     success (res) {
-  //       console.log(res.windowHeight);
-  //       height = res.windowHeight
-  //     }
-  //   })
-  //   if (app.globalData.userInfo === null){
-  //     that.setData({
-  //       login:true
-  //     })
-  //   }else{
-  //     that.setData({
-  //       login: false,
-  //       height:height
-  //     })
-  //   }
-  // },
-
+  onPullDownRefresh: function() {
+    this.requestData()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.requestData()
+  },
+  requestData: function(){
     var that = this;
-    var url = 'http://118.114.244.93:60226/student/select-doctor-classcode?classcode=115';
-
+    var url = 'http://118.114.244.93:60227/user/getRank';
+    var urlTarget = 'http://118.114.244.93:60227/user/getTarget';
+    var urlCurrent = 'http://118.114.244.93:60227/user/getSummary';
+    var tartgetMoney=1;
+    var currentMoney=0;
+    var progressTxt=0;
     // 调用的是自己封装的工具函数，在utils中
     utils.myRequest({
       url:url,
@@ -53,10 +36,9 @@ Page({
       success:function(result){
         that.setData({
           showitem:true,
-    
           rankcontent:result.data.data,
-          time_pro:Number(parseFloat(22589/10000*10).toFixed(1)),
           
+          // progressTxt:Number(parseFloat(22589/10000*10).toFixed(1)),
         })
       },
       fail:function(){
@@ -65,6 +47,23 @@ Page({
         })
       }
     });
+    utils.myRequest({
+      url:urlCurrent,
+      methods:'GET',
+      success:function(result){
+        that.setData({
+          currentMoney:result.data.data.summary,
+        })
+      
+      },
+      fail:function(){
+        that.setData({
+          // showitem:false
+        })
+      }
+    });
   },
 
 })
+
+
